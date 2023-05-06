bfc1662fbe5e910ef45ace29e65b9d0355d8da12
Gulsher-19I-0445 <gulsherkhan0707@gmail.com>
2023-05-06 18:35:59 +0500

    Added Login page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
-
+import LogIn from './Pages/Login';
+import {BrowserRouter, Route, Routes } from "react-router-dom"
+import Dashboard from './Pages/Dashboard';
+import AdminDashboard from './Pages/AdminDashboard';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+    
+      <BrowserRouter>
+      
+    <Routes>
+        <Route exact path='/' element = {<LogIn/>}></Route>
+        <Route exact path='/Home' element = {<AdminDashboard></AdminDashboard>}></Route>
+        
+        
+    </Routes>
+
+    </BrowserRouter>
+
+      
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Pages/AdminDashboard.js
@@ -0,0 +1,7 @@
+import AdminNavbar from "../Components/adminNavbar";
+
+export default function AdminDashboard(){
+    return(
+        <AdminNavbar></AdminNavbar>
+    )
+}
