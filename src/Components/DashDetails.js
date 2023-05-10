ec8f514b376542df4646397b3442f511e0b662c3
Gulsher-19I-0445 <gulsherkhan0707@gmail.com>
2023-05-10 22:18:46 +0500

    Integrated backend and added funtionality for adding courses and learners

--- src/Components/DashDetails.js
+++ src/Components/DashDetails.js
@@ -31,12 +31,12 @@ const people = [
   export default function DashDetails() {
     return (
       <ul role="list" className="divide-y divide-gray-100">
-        {people.map((person) => (
-          <li key={person.email} className="flex justify-between gap-x-6 py-5">
+        
+          <li key='1' className="flex justify-between gap-x-6 py-5">
             <div className="flex gap-x-4">
-              <img className="h-12 w-12 flex-none rounded-full bg-gray-50" src={person.imageUrl} alt="" />
+              <img className="h-12 w-12 flex-none rounded-full bg-gray-50" src='https://cdn-icons-png.flaticon.com/512/4762/4762232.png' alt="" />
               <div className="min-w-0 flex-auto">
-                <p className="text-sm font-semibold leading-6 text-gray-900">{person.name}</p>
+                <p className="text-sm font-semibold leading-6 text-gray-900">Number of Enrolled Courses</p>
                 
               </div>
             </div>
@@ -56,7 +56,55 @@ const people = [
               )} */}
             </div>
           </li>
-        ))}
+          <li key='2' className="flex justify-between gap-x-6 py-5">
+            <div className="flex gap-x-4">
+              <img className="h-12 w-12 flex-none rounded-full bg-gray-50" src='https://www.clipartmax.com/png/middle/309-3099036_completed-course-finished-work-icon-download.png' alt="" />
+              <div className="min-w-0 flex-auto">
+                <p className="text-sm font-semibold leading-6 text-gray-900">Number of Completed Courses</p>
+                
+              </div>
+            </div>
+            <div className="hidden sm:flex sm:flex-col sm:items-end">
+              <p className="text-sm leading-6 text-gray-900">{/* props go here */}13</p>
+              {/* {person.lastSeen ? (
+                <p className="mt-1 text-xs leading-5 text-gray-500">
+                  Last seen <time dateTime={person.lastSeenDateTime}>{person.lastSeen}</time>
+                </p>
+              ) : (
+                <div className="mt-1 flex items-center gap-x-1.5">
+                  <div className="flex-none rounded-full bg-emerald-500/20 p-1">
+                    <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
+                  </div>
+                  <p className="text-xs leading-5 text-gray-500">Online</p>
+                </div>
+              )} */}
+            </div>
+          </li>
+          <li key='3' className="flex justify-between gap-x-6 py-5">
+            <div className="flex gap-x-4">
+              <img className="h-12 w-12 flex-none rounded-full bg-gray-50" src='https://static.vecteezy.com/system/resources/previews/000/593/213/original/vector-assignment-icon.jpg' alt="" />
+              <div className="min-w-0 flex-auto">
+                <p className="text-sm font-semibold leading-6 text-gray-900">Number of Assignments</p>
+                
+              </div>
+            </div>
+            <div className="hidden sm:flex sm:flex-col sm:items-end">
+              <p className="text-sm leading-6 text-gray-900">{/* props go here */}13</p>
+              {/* {person.lastSeen ? (
+                <p className="mt-1 text-xs leading-5 text-gray-500">
+                  Last seen <time dateTime={person.lastSeenDateTime}>{person.lastSeen}</time>
+                </p>
+              ) : (
+                <div className="mt-1 flex items-center gap-x-1.5">
+                  <div className="flex-none rounded-full bg-emerald-500/20 p-1">
+                    <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
+                  </div>
+                  <p className="text-xs leading-5 text-gray-500">Online</p>
+                </div>
+              )} */}
+            </div>
+          </li>
+        
       </ul>
     )
   }
--- src/Components/cards.js
+++ src/Components/cards.js
@@ -1,32 +1,44 @@
 export default function MyCards(props) {
+
+  function goto(){
+    console.log('h')
+  }
   //console.log(props.m1);
   return (
     <>
-      <div class="max-w-md mx-10 rounded overflow-hidden shadow-lg m-10 bg-White rounded-3xl">
-        <div class="text-Black font-SF text-xl mb-2 m-2">{props.name}</div>
+    
+    <button onClick={goto} className="max-w-md mx-10 rounded overflow-hidden shadow-lg m-10 bg-White rounded-3xl">
+        <div class="text-Black font-SF text-xl mb-2 m-2 font-bold">{props.name}</div>
         {/* <img class="w-full p-3 rounded-3xl" src="" alt="Sunset in the mountains" /> */}
         <div class="bg-backGray m-2 p-px rounded-xl">
-          <div class="px-6 py-4 flex bg-gray-1 m-3 divide-x-[2px] rounded-xl">
+          <div class="px-6 py-4 flex bg-gray-1 rounded-xl">
 
-            <p class="text-gray-700 text-base text-Black">
-              Students enrolled
-            </p>
-            <p class="text-gray-700 text-base text-Black ml-auto px-2 truncate w-28 overflow-hidden">
-              {/* {props.owner_Key} */}50
+            
+            <p class="text-gray-700 text-base text-Black px-2 truncate w-28 overflow-hidden">
+              {props.desc} 
             </p>
           </div>
           <div class="px-6 py-4 flex bg-gray-1 m-3 divide-x-[2px] rounded-xl">
 
             <p class="text-gray-700 text-base text-Black">
-              Due Assignments
+              Start Date
             </p>
-            <p class="text-gray-700 text-base text-Black ml-auto px-2 block truncate w-28 overflow-hidden">
-              2
+            <p class="text-gray-700 text-base text-Black ml-auto px-2 block  w-28 overflow-hidden">
+              {props.start.split('T')[0]}
+            </p>
+          </div>
+          <div class="px-6 py-4 flex bg-gray-1 m-3 divide-x-[2px] rounded-xl">
+
+            <p class="text-gray-700 text-base text-Black">
+              End Date
+            </p>
+            <p class="text-gray-700 text-base text-Black ml-auto px-2 block  w-28 overflow-hidden">
+              {props.end.split('T')[0]}
             </p>
           </div>
           
         </div>
-      </div>
+      </button>
     </>
 
   );
