5f18e7b7c94540c2d34a57ecdde9833b78819a98
Gulsher-19I-0445 <gulsherkhan0707@gmail.com>
2023-05-10 23:45:44 +0500

    submissoion

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import LogIn from './Pages/Login';
 import {BrowserRouter, Route, Routes } from "react-router-dom"
-import Dashboard from './Pages/Dashboard';
+// import Dashboard from './Pages/Dashboard';
 import AdminDashboard from './Pages/AdminDashboard';
 import CourseDash from './Pages/CoursesDashboard';
 import CourseAdd from './Pages/addCourse';
 import LearnerAdd from './Pages/addLearner';
+import CourseHome from './Pages/CourseHome';
 function App() {
   return (
     <div>
@@ -19,6 +20,7 @@ function App() {
         <Route exact path='/Courses' element = {<CourseDash></CourseDash>}></Route>
         <Route exact path='/AddCourses' element = {<CourseAdd></CourseAdd>}></Route>
         <Route exact path='/Addlearner' element = {<LearnerAdd></LearnerAdd>}></Route>
+        <Route exact path='/CourseHome' element = {<CourseHome></CourseHome>}></Route>
         
         
     </Routes>
