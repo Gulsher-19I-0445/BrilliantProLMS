176c5f0db7e8c07412eaa97c88f2ff627d3f1818
Gulsher-19I-0445 <gulsherkhan0707@gmail.com>
2023-05-13 19:30:53 +0500

    Added create assignment

--- backend/index.js
+++ backend/index.js
@@ -31,8 +31,29 @@ const courseSchema = new mongoose.Schema({
     enrolled:Number,
   });
 
+  const announceSchema = new mongoose.Schema({
+    CourseID:String,
+      Announcetext:String,
+    });
+
+    const questSchema = new mongoose.Schema({
+      title: String,
+      questions: [{
+        text: String,
+        options: [{
+          text: String,
+          value: String,
+          checked: Boolean,
+        }],
+      }],
+    });
+    
+    // Define a model for surveys
+    const MCQs = mongoose.model('MCQs', questSchema);
+
 const Learner = mongoose.model('Learner', learnerSchema);
 const Course = mongoose.model('courses', courseSchema);
+const Announce = mongoose.model('announce', announceSchema);
 //##########################################################################################
 app.post('/addLearner', function(req, res) {
   const learner = new Learner({
@@ -144,9 +165,90 @@ app.get('/GetCourses', function(req, res) {
         res.status(500).send('An error occurred while retrieving courses from the database');
       });
   });
+//-----------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------------
+app.post('/addAnnouncement', function(req, res) {
+  const n = req.query.name;
+  const announce = new Announce({
+    CourseID:n,
+    Announcetext:req.body.announce
+  });
+
+  announce.save()
+  .then(() => {
+    console.log('Announce added to database');
+    res.send('Announce added to database');
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('An error occurred while adding the Announce to the database');
+  });
+
+});
+
+// Create the database and learners collection if they don't already exist
+mongoose.connection.once('open', function() {
+  mongoose.connection.db.listCollections({name: 'announce'})
+    .next(function(err, info) {
+      if (!info) {
+        mongoose.connection.db.createCollection('announce', function(err, collection) {
+          if (err) throw err;
+          console.log("announce collection created!");
+        });
+      }
+    });
+});
 
 
+//-----------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------------
 
+app.get('/GetAnnounce', function(req, res) {
+  const n = req.query.name;
+  console.log(n)
+  Announce.find({CourseID:n})
+    .then(function(announce) {
+      res.send(announce);
+    })
+    .catch(function(err) {
+      console.error(err);
+      res.status(500).send('An error occurred while retrieving courses from the database');
+    });
+});
+//--------------------------------------------------------
+
+app.post('/MCQ', async (req, res) => {
+  try {
+    // Parse the survey data from the request body
+    const { title, questions } = req.body;
+
+    // Create a new survey document
+    const survey = new MCQs({ title, questions });
+
+    // Save the survey to the database
+    await survey.save();
+
+    // Send a success response
+    res.status(201).json({ message: 'Survey created successfully' });
+  } catch (error) {
+    // Send an error response
+    res.status(500).json({ error: error.message });
+  }
+});
+
+//---------------------------------------------------------
+app.get('/getMCQ', function(req, res) {
+  //const n = req.query.name;
+  //console.log(n)
+  MCQs.find()
+    .then(function(MCQ) {
+      res.send(MCQ);
+    })
+    .catch(function(err) {
+      console.error(err);
+      res.status(500).send('An error occurred while retrieving courses from the database');
+    });
+});
 app.listen(8000, function() {
   console.log(`Server running on port 8000`);
 });
